ac6fe1ef5f2189b8b33914037080c02cbfb357cf
LukeSchaefer <lukes@Lukes-MacBook-Pro.local>
2022-10-08 15:24:27 -0500

    changed minimax to fit the psudocode better - still not done

--- src/minimax.py
+++ src/minimax.py
@@ -18,58 +18,78 @@ def calculateHn(gameBoard, xo):
         hn += 5*(gameBoard.oneSideOpen2for0) - 2*(gameBoard.oneSideOpen2forX)
     return hn
 
-#node class
-#unfinished expand()
+#node for minimax tree
 class Node:
-    def __init__(self, gameBoard, move, minOrMax, parent):
+    def __init__(self, gameBoard, move, parent):
+        #state is the current board at the moment
         self.state = GameBoard(gameBoard)
+        #the move it takes to get to it's state
         self.move = move
+        #if the current state is a terminal board (win or loss)
         self.terminal = True if gameBoard.winner == 0 else False
-        self.minOrMax = minOrMax
-        self.hn = -1000 if minOrMax == True else 1000 
+        #hn is null at before it knows what the best move for it's current state
+        self.hn = None 
+        #children nodes (all possible moves from current state)
         self.children = []
+        #parent node
         self.parent = parent
+        #determines it's depth for search purposes (2ply vs 4ply etc)
         self.depth = parent.depth + 1
 
-    #unfinished
-    def expand(self):
+    #adds the children of the node
+    def expand(self, xo):
+        #for every open slot in the current state, there is a move that is available for X or O
+        #expanding from an X move results in an O move, and vise versa
         for row in range(self.state.rowCount):
             for col in range(self.state.colCount):
+                #if a space is open in the current state, fill state space, and add that state with that filled space as a child
                 if self.state.board[row][col] == 0:
                     newBoard = GameBoard(self.state)
-                    newBoard.board[row][col] = self.xo
-                    self.children.append(Node(newBoard, (row, col), False if self.minOrMax == True else False, self))
+                    newBoard.board[row][col] = xo
+                    #state of child has the possible node filled, the move that would get to that state, and is the opposite of it's parent (min or max)
+                    self.children.append(Node(newBoard, (row, col), self))
 
         
 
+def minimaxSearch(state, xo):
 
-#whoami is the starting node who originally calls the function (player X or player O)- allows for min and max switching - 
-#maxOrMin is the level currently at - True = Max, False = min
-#recursively finds the best move for the current node 
-#UNFINISHED
-def minimaxSearch(node, maxDepth, maxOrMin, whoami):
+    root = Node(state, None, None)
+    root.expand(xo)
 
-    score = 0
-    move = (0,0)
+    move = maxValueSearch(root)
 
-    if node.depth < maxDepth:
-        node.expand()
-        smallest = 10000
-        largest = -10000
-        v = 0
-        for child in node.children:
-            minimaxSearch(child, maxDepth-1, False if maxOrMin == True else True)
-            if maxOrMin == True:
-                v = calculateHn(child.state, whoami)
-            else: 
-                v = calculateHn(child.state, whoami * -1)
-            
-            if v < smallest:
-                smallest = v
-            if v > largest:
-                largest = v
-        node.score = (largest if maxOrMin == True else smallest)
-        node.move = node.move
+    return move
+
+def maxValueSearch(node):
+    #unsure what terminal actually means
+    if node.terminal == True:
+        return 1000
+    
+    largest = -1000
+    v = -1000
+    move = None
+    for child in node.children:
+        v = minValueSearch(child)
+        if v > largest:
+            v = largest
+            move = child.move
+    return (v, move)
+
+def minValueSearch(node):
+
+    #unsure what terminal actually means
+    if node.terminal == True:
+        return -1000
+    smallest = 1000
+    v = 1000
+    move = None
+    for child in node.children:
+        v = maxValueSearch(child)
+        if v < smallest:
+            v = smallest
+            move = child.move
+    return (v, move)
+
 
 
 
