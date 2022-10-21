84bfb92796d01066f49bebfbd321b471e3f89c8e
oshouse <55266023+oshouse@users.noreply.github.com>
2022-10-21 00:50:02 -0500

    updated the game class

--- src/player.py
+++ src/player.py
@@ -7,7 +7,8 @@ class Player:
         # Player Name
         # Player Symbol
     
-    def __init__(self, movesAhead, playerName, playerSymbol):
+    def __init__(self, movesAhead, playerName, playerSymbol, firstMove):
         self.movesAhead = movesAhead
         self.playerName = playerName
         self.playerSymbol = playerSymbol
+        self.firstMove = firstMove
